38695f9a4d3be19b3845b02668809bc8c97c75e3
Jasper Ngo <ngojasperryan@gmail.com>
2025-06-19 04:36:13 +0800

    feat: add new assets and update Navbar component with image placeholder

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,7 @@ const Navbar: React.FC = () => {
     return (
         <nav className="bg-gray-800 text-white py-4 px-6 flex justify-between items-center">
             <div className="text-lg font-semibold">My Portfolio</div>
+            <img src="" alt="" />
             <ul className="flex space-x-4">
                 <li><a href="#about" className="hover:text-blue-500">About</a></li>
                 <li><a href="#projects" className="hover:text-blue-500">Projects</a></li>
