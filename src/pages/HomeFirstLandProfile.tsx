1052ee24fd480c45a88d826f1877fccd44f636ee
Jasper Ngo <ngojasperryan@gmail.com>
2025-06-20 08:14:43 +0800

    feat: enhance HomeFirstLandProfile with BoxReveal components for animated text and improved layout

--- src/pages/HomeFirstLandProfile.tsx
+++ src/pages/HomeFirstLandProfile.tsx
@@ -1,4 +1,5 @@
 import personalProfile from "../assets/jasperImageForPort.png";
+import { BoxReveal } from "../components/magicui/box-reveal";
 
 const HomeFirstLandProfile = () => {
   return (
@@ -9,21 +10,48 @@ const HomeFirstLandProfile = () => {
       {/* Grid Section */}
       <div className="grid grid-cols-4 h-screen">
         {/* Image Section */}
-        <div className="col-span-2">
-          <img
+              <div className="col-span-2 ">
+                  <div className="ml-10 pl-20 w-full h-full relative">
+                      <img
             src={personalProfile}
             alt="Jasper's profile"
             className="w-full h-full object-cover -mt-30 "
           />
+                  </div>
+          
         </div>
 
         {/* Text Section */}
-        <div className="col-span-2 flex items-center justify-center p-8">
-        <p className="text-xl text-center font-serif opacity-0 animate-fade-in">
-  Dedicated full-stack developer specializing in building robust and user-centric web applications with clean, maintainable code.
-</p>
+              <div className="col-span-2 flex flex-col items-start justify-center p-8 space-y-6 bg-black ">
+                  <div className="ml-10 pl-20">
+                      
+                     <BoxReveal boxColor={"#14b8a6"} duration={0.5}>
+    <p className="text-[3.5rem] font-semibold text-white">
+      Hi, I’m Jasper<span className="text-[#22d3ee]">.</span>
+    </p>
+  </BoxReveal>
 
-        </div>
+  <BoxReveal boxColor={"#14b8a6"} duration={0.5}>
+    <h2 className="text-[1.2rem] text-gray-300">
+      A Quiet Builder Passionate About Coding
+    </h2>
+  </BoxReveal>
+
+  <BoxReveal boxColor={"#22d3ee"} duration={0.5}>
+    <div className="text-gray-400 text-base max-w-lg leading-relaxed">
+      <p>
+        I craft clean, efficient web applications using React, TypeScript, and Tailwind CSS. <br />
+        Constantly learning new technologies and improving my skills. <br />
+        Focused on creating smooth user experiences with clean code. <br />
+        Let’s build something great together.
+      </p>
+    </div>
+  </BoxReveal> 
+                  </div>
+  
+</div>
+
+
       </div>
 
      
